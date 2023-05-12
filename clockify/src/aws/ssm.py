1339b65e2a14dcb2a6a7860b63c7b01155aa8c1b
gabryuri <gyribeiro2014@gmail.com>
2023-05-12 18:17:06 -0300

    feat: add data fetching and logging

--- clockify/src/aws/ssm.py
+++ clockify/src/aws/ssm.py
@@ -1,12 +1,14 @@
 import boto3
 
 class ParameterStoreFetcher:
-	def __init__(self, region):
+	def __init__(self, region, logger):
 		self.__region = region
 		self.__client = boto3.client('ssm', region_name=self.__region)
+		self.__logger = logger
 
 	# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm/client/get_parameters.html
 	def fetch_parameter_value(self, param_name, decryption=True):
+		self.__logger.info(f"retrieving parameter {param_name}")
 		return self.__client.get_parameter(
 			Name=param_name,
 			WithDecryption=decryption
--- clockify/src/lambda_handler.py
+++ clockify/src/lambda_handler.py
@@ -1,15 +1,22 @@
-import requests as r
+import requests
 import json
 import pprint
 import os
+import logging
+import datetime 
 
 from aws.ssm import ParameterStoreFetcher
 
+logging.basicConfig(format="[%(levelname)s][%(asctime)s][%(filename)-15s][%(lineno)4d][%(threadName)10s] - %(message)s", level=logging.INFO, force=True)
+logger = logging.getLogger()
+
 class Interactor:
-	def __init__(self, workspace_id):
-		self.__fetcher = ParameterStoreFetcher('us-east-1')
+	def __init__(self, workspace_id, logger):
+		self.__fetcher = ParameterStoreFetcher('us-east-1', logger)
 		self.__api_key = self.__fetcher.fetch_parameter_value('prd-credentials.clockify.api-key')
 		self.__workspace_id = workspace_id
+		self.__api_date_format = "%Y-%m-%dT%H:%M:%S.%f%zZ"
+		self.__logger = logger
 
 	@property
 	def base_endpoint(self):
@@ -19,24 +26,38 @@ class Interactor:
 	def detailed_reports_endpoint(self):
 		return (os.path.join(self.base_endpoint.strip('/'), 'reports/detailed'))
 
+	def generate_headers(self):
+		headers = {'x-api-key': self.__api_key,
+   				   'content-type': 'application/json'}
+		return headers
+
+	def generate_time_filters(self, end_date, interval_days):
+		end_date_object = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+		begin_date_object = end_date_object - datetime.timedelta(days=interval_days)
+		
+		date_filter = {'dateRangeStart': begin_date_object.strftime(self.__api_date_format),
+					   'dateRangeEnd': end_date_object.strftime(self.__api_date_format),
+					   'detailedFilter': {}}
+
+		self.__logger.info(f"Date filter generated: from {begin_date_object} to {end_date_object}")
+
+		return date_filter
+
+
+	def fetch_detailed_reports(self, end_date=str(datetime.datetime.today()), interval_days=7):
+		self.__logger.info(f"Retrieving detailed reports data.")
+		headers = self.generate_headers()
+		filters = self.generate_time_filters(end_date, interval_days)
+		response = requests.post(self.detailed_reports_endpoint, headers=headers, json=filters)
+		return response.text, response.status_code
+
 
 workspace_id = '5e95c064ea8094116e8e0a56'
-
-# header = {
-#     'x-api-key': api_key
-#     , 'content-type': 'application/json'
-# }
-
-# response = json.loads(r.post(url=f'https://reports.api.clockify.me/v1/workspaces/{workspace_id}/reports/detailed'
-#                              , headers=header
-#                              , json={
-#                                  'dateRangeStart': '2023-01-10T00:00:00.000Z'
-#                                  , 'dateRangeEnd': '2023-01-21T00:00:00.000Z'
-#                                  , "detailedFilter": {}
-#                              }).text)
-# pprint.pprint(response)
-
-a = Interactor(workspace_id)
-print(a.base_endpoint)
-print(a.detailed_reports_endpoint)
-print(a.api_key)
+api_interactor = Interactor(workspace_id, logger)
+print(api_interactor.base_endpoint)
+print(api_interactor.detailed_reports_endpoint)
+# date_filter = api_interactor.generate_time_filters(end_date='2023-05-12', interval_days=7)
+# print(date_filter)
+data, status = api_interactor.fetch_detailed_reports(end_date='2023-05-12', interval_days=7)
+print(status)
+# print(data)
